fc230c7ccb89cdcca46073c32e22e6545dfa2f38
Nikita Zorin <iiispikeriii@gmail.com>
2015-08-02 11:15:41 +0300

    add flickr

---
+++ src/Flickr.php
@@ -0,0 +1,113 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: spiker
+ * Date: 13.02.15
+ * Time: 20:13
+ */
+
+require_once __DIR__. '/A_Engine.php';
+
+class Flickr extends A_Engine {
+
+    const API_VERSION = '5.28';
+
+    public $back_url;
+    public $scope = 'basic';//basic+comments+relationships+likes
+    public $urls = array(
+        'auth_url' => 'https://api.instagram.com/oauth/authorize/?client_id=%s&redirect_uri=%s&response_type=code',
+        'access_url' => 'https://www.flickr.com/services/oauth/request_token',
+        'link' => 'https://api.flickr.com/services/rest/?method=flickr.test.echo&name=value',
+    );
+
+    private function getOAuthParameters()
+    {
+        return array(
+            'oauth_nonce' => md5(microtime() . mt_rand()),
+            'oauth_timestamp' => time(),
+            'oauth_consumer_key' => FLICKR_API_KEY,
+            'oauth_signature_method' => 'HMAC-SHA1',
+            'oauth_version' => '1.0',
+        );
+    }
+
+    public function authenticate() {
+
+        if ($this->isAuth()) return;
+
+        if (isset($_GET['code'])) {
+            $this->getAccessToken();
+        } else {
+            $_SESSION['back_url'] = 'http://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
+            $this->getRequestToken();
+        }
+    }
+
+    protected function getRequestToken(){
+
+        $url = $this->getAuthUrl();
+
+        header('Location:'.$url);
+        exit;
+
+    }
+
+    protected function getAccessToken(){
+
+        if(!isset($_GET['code'])) throw new SocException('Not find "code"');
+
+        $url = $this->getAccessUrl($_GET['code']);
+
+        $query = parse_url($url);
+        parse_str($query['query'], $params);
+        $query_url = $query['scheme'].'://'.$query['host'].$query['path'];
+
+        $curl = new Curl\Curl();
+        $resp = $curl->post($query_url, $params);
+
+        if(isset($resp->error_message)) throw new SocException($resp->error_message, $resp->code);
+
+        $this->setToken($resp->access_token);
+
+
+        header('Location:'.$_SESSION['back_url']);
+        exit;
+    }
+
+    /**
+     * get Url for auth
+     * @return string
+     */
+    protected function getAuthUrl(){
+
+        return sprintf($this->urls['auth_url'], $this->config['app_id'], $_SESSION['back_url']);
+    }
+
+    /**
+     * get Url for access token
+     * @return string
+     */
+    protected function getAccessUrl($code){
+
+        return sprintf($this->urls['access_url'], $this->config['app_id'], $this->config['secret_key'], $_SESSION['back_url'],$code);
+    }
+
+    public function run($method, $data = array(), $auth = false, $type = 'get'){
+
+        $method = trim($method, '/');
+
+        if($auth) {
+            $this->authenticate();
+            $data['access_token'] = $this->getToken();
+        }
+
+        $url = $this->urls['link'].$method;
+
+        $curl = new Curl\Curl();
+        $resp = $curl->$type($url, $data);
+
+        return $resp;
+    }
+
+}
